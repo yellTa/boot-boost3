bb5bc9529a1e50f50a175d2b1355a15cfa09b79d
YellTa <>
2024-12-12 00:25:21 +0900

    test(예약 저장하기) : table index 적용후 소모되는 시간을 확인
    소모시간 확인을 위해 @BeforeAll, @AfterAll을 사용해서 로직을 수행하는데 걸리는 시간 확인
    
    index 사용 후에 약 5000ms정도 성능이 개선됨

--- src/test/java/com/example/springboot3/web/ReservationControllerTest.java
+++ src/test/java/com/example/springboot3/web/ReservationControllerTest.java
@@ -1,51 +1,66 @@
 package com.example.springboot3.web;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.*;
 
-import com.example.springboot3.Dto.JPAEntity.ReservationInfoPriceDTO;
-import com.example.springboot3.Dto.PricesRequestDTO;
-import com.example.springboot3.Dto.ReservationInfosRequestDTO;
-import com.example.springboot3.Dto.response.ReservationInfoResponseDTO;
-import com.example.springboot3.service.ReservationService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.springboot3.Dto.PricesRequestDTO;
+import com.example.springboot3.Dto.ReservationInfosRequestDTO;
+import com.example.springboot3.Dto.response.ReservationInfoResponseDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 @Transactional
 class ReservationControllerTest {
 
-    @Autowired
-    private ReservationService reservationService;
+	private static long startTime;
+	@Autowired
+	private ReservationController reservationController;
 
-    @Test
-    @DisplayName("실제 DB 연동해서 결과확인")
-    public void testSaveReservation() throws JsonProcessingException {
-        // Given: 실제 데이터 생성
-        ReservationInfosRequestDTO request = new ReservationInfosRequestDTO(
-            1, LocalDate.now(), 1, 1, List.of(new PricesRequestDTO(6, 2)));
+	@BeforeAll
+	public static void startTest() {
+		startTime = System.nanoTime();
+	}
 
-        // When: 서비스 계층을 호출
-        ReservationInfoResponseDTO response = reservationService.saveAndReturnReservationResponse(request);
-        ObjectMapper objectMapper = new ObjectMapper();
+	@AfterAll
+	public static void endTest() {
+		long endTime = System.nanoTime();
+		System.out.println("총 수행 시간 = " + (endTime - startTime) / 1_000_000 + " ms");
+	}
 
-        // Then: 데이터베이스에 값이 저장되었는지 확인
-        assertNotNull(response);
-        assertEquals(response.getProductId(), 1);
-        assertEquals(response.getCancelFlag(), 0);
-        assertEquals(response.getDisplayInfoId(), 1);
-        assertEquals(response.getUserId(), 1);
-        List<ReservationInfoPriceDTO> prices = response.getPrices();
-        assertEquals(prices.getFirst().getProductPriceId(), 2);
-        assertEquals(prices.getFirst().getCount(), 6);
+	@Test
+	@DisplayName("실제 DB 연동해서 결과확인")
+	public void testSaveReservation() throws JsonProcessingException {
+		// Given: 실제 데이터 생성
+		ReservationInfosRequestDTO request = new ReservationInfosRequestDTO(1, LocalDate.now(), 2, 1, List.of(new PricesRequestDTO(3, 2)));
 
-    }
+		// When: 서비스 계층을 호출
+		Object body = reservationController.doReservation(request)
+										   .getBody();
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		String jsonString = objectMapper.writeValueAsString(body);
+		// System.out.println(jsonString);
+		ReservationInfoResponseDTO res = objectMapper.readValue(jsonString, ReservationInfoResponseDTO.class);
+
+		// Then: 데이터베이스에 값이 저장되었는지 확인
+		assertThat(res.getProductId()).isEqualTo(1);
+		assertThat(res.getCancelFlag()).isEqualTo(0);
+		assertThat(res.getDisplayInfoId()).isEqualTo(2);
+		assertThat(res.getUserId()).isEqualTo(1);
+		assertThat(res.getPrices()
+					  .getFirst()
+					  .getCount()).isEqualTo(3);
+	}
 
 }
