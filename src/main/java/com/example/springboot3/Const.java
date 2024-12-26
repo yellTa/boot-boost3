5790554fbef61e2827be96b2e721f95f9574b710
YellTa <>
2024-12-26 21:53:16 +0900

    feat (파일 로드) : 파일을 로드하는 롲기 구현
    TableAlias.java =====
    jooq : File Info table 별칭 지정
    
     ImageService.java=====
     imagePath를 리턴하는 로직 구성
    
     FileDao.java =====
     file_id에 해당하는 file경로 지정
    
     ImageController.java =====
     파일 이미지Controller 설정 이미지를 body에 뿌림
    
     Const.java=====
     파일 소스 위치 상수 지정

--- src/main/java/com/example/springboot3/Const.java
+++ src/main/java/com/example/springboot3/Const.java
@@ -5,6 +5,7 @@ public class Const {
 	public static final int NO_CATEGORY = 0;
 	public static final String MAIN_PICTURE = "ma";
 	public static final int CANCELLED = 1;
+	public static final String IMAGE_PATH = "src/main/resources/";
 
 	private Const() {
 	}
---
+++ src/main/java/com/example/springboot3/web/ImageController.java
@@ -0,0 +1,40 @@
+package com.example.springboot3.web;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot3.service.ImageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/files")
+@RequiredArgsConstructor
+public class ImageController {
+
+	private final ImageService imageService;
+
+	@GetMapping
+	public ResponseEntity<?> getFile(@RequestParam("fileId") int fileId) {
+
+		Path filePath = imageService.getFilePath(fileId);
+		Resource resource = null;
+		try {
+			resource = new UrlResource(filePath.toUri());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+		return ResponseEntity.ok()
+							 .contentType(MediaType.IMAGE_JPEG)
+							 .body(resource);
+	}
+}
